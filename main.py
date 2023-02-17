27bbf006f99afa2760d042f9066b123242a3dc86
npcrowder <124738176+npcrowder@users.noreply.github.com>
2023-02-17 04:56:33 +0000

    Added a bunch more stuff

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-print('Hello World! Bellevue family!')
-x = input("What is your favorite animal?")
-if x == "yes":
- print ("yes is not an animal")
- input ("Could you enter an animal now?")
+def main():
+ print('Hello World! Bellevue family!')
+ x = input("What is your favorite animal?")
+ if x == "yes":
+  print ("yes is not an animal")
+  y = input ("Could you enter an animal now?")
+  if y == "yes":
+    main()
+ else:
+  print ("I love " + x + "s. ")
+  z = input("Do you like any other animals?")
+  if z == "yes":
+    main()
+main()
