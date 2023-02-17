c31d6dd4749ff9234fbf92d0a507be1c8371701e
npcrowder <124738176+npcrowder@users.noreply.github.com>
2023-02-17 04:44:32 +0000

    Added an if statement and an input

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 print('Hello World! Bellevue family!')
-input("What is your favorite animal?")
+x = input("What is your favorite animal?")
+if x == "yes":
+ print ("yes is not an animal")
+ input ("Could you enter an animal now?")
